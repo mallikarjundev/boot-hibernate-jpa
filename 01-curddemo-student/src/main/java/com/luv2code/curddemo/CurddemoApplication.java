6350cbae8480db93e6ff26a3bc10076df5421e21
arjunoggu <mallikarjundev2@gmail.com>
2023-10-23 20:28:26 -0400

    Database setup

---
+++ 01-curddemo-student/src/main/java/com/luv2code/curddemo/CurddemoApplication.java
@@ -0,0 +1,21 @@
+package com.luv2code.curddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CurddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args){
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+}
